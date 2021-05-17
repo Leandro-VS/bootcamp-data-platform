f14c41393fd1298fde89cd366863b17b173d85a6
Leandro-VS <leandro.vieira.silva@usp.br>
2021-05-17 07:36:34 -0300

    cria dms

--- app.py
+++ app.py
@@ -5,12 +5,19 @@ from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.common_stack import CommonStack
 from data_platform.kinesis.stack import KinesisStack
+from data_platform.dms.stack import DmsStack
 
 
 app = core.App()
 
 data_lake_stack = DataLakeStack(app)
 common_stack  = CommonStack(app)
-kinesis_stack = KinesisStack(app, data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket)
+kinesis_stack = KinesisStack(app, 
+                            data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket
+                            )
+dms_stack = DmsStack(app, 
+                    common_stack=common_stack, 
+                    data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket
+                    )
 
 app.synth()
